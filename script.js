9acc607e5af1332458defe67b507b561b31d9cb0
Aleksandr <aleksandr.elovskih04@gmail.com>
2023-10-16 22:51:42 +0500

    Поключено сохранение результатов вычислений в массив

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 let result = document.getElementById('result');
 let answer = document.querySelector('.answer');
+let save = [];
+console.log(save)
 
 result.addEventListener('click', function () {
    let checkedBtn = document.querySelector('.radio-btn:checked');
-   let firstNum = document.getElementById('firstNum');
-   let secondNum = document.getElementById('secondNum');
+   let firstInput = document.getElementById('firstNum');
+   let secondInput = document.getElementById('secondNum');
+   let firstNum = firstInput.value;
+   let secondNum = secondInput.value;
+   let checkBtnFormassiv = checkedBtn.value;
+
+   save.push (
+      {firstNum, secondNum, checkBtnFormassiv, answer},
+   );
 
    if (checkedBtn.value === '+') {
-      answer.textContent = Number(firstNum.value) + Number(secondNum.value);
+      answer.textContent = Number(firstNum) + Number(secondNum);
    };
    if (checkedBtn.value === '-') {
-      answer.textContent = Number(firstNum.value) - Number(secondNum.value);
+      answer.textContent = Number(firstNum) - Number(secondNum);
    };
    if (checkedBtn.value === '*') {
-      answer.textContent = Number(firstNum.value) * Number(secondNum.value);
+      answer.textContent = Number(firstNum) * Number(secondNum);
    };
    if (checkedBtn.value === '/') {
-      answer.textContent = Number(firstNum.value) / Number(secondNum.value);
+      answer.textContent = Number(firstNum) / Number(secondNum);
    };
 
+   console.log(save);
 });
